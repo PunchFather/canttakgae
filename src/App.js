d3bb6c0b12a84f13fb59977e816f9b2a9385417a
taemin <zhfhldp1@naver.com>
2022-08-31 00:28:22 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  useEffect(() => {
+    console.log("시작");
+    return () => {
+      console.log("끝");
+    };
+  }, []);
+
+  const can = ["오리젠", "로얄캐닌", "뉴트리", "등등등", "등등등"];
+
+  const [edit, setIsedit] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const toggleIsEdit = () => setIsedit(!edit);
+  const content = () => {
+    <div>
+      <img src="assets/cat.jpg" />{" "}
+    </div>;
+  };
+
+  const random = can[Math.floor(Math.random() * can.length)];
+
+  const stop = () => {
+    toggleIsEdit();
+    setCount(count + 1);
+    if (count >= 5) {
+      setCount(0);
+    }
+    console.log(count);
+    return (
+      <div className="App">
+        <button onClick={stop}>눌러보라 냥😾</button>
+        <div>{edit && random}</div>
+        <div>{edit && content}</div>
+        {content}
+      </div>
+    );
+  };
 }
-
-export default App;
